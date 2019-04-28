ae472a14760e4bc4c46176034c570d4b84e05ffb
kj_cho <kj_cho@idealidea.co.kr>
2019-04-28 16:01:31 +0900

    데이터 좀 그럴싸하게 변경

--- app/src/main/java/com/tj/pizzastore02/MainActivity.java
+++ app/src/main/java/com/tj/pizzastore02/MainActivity.java
@@ -46,12 +46,12 @@ public class MainActivity extends BaseActivity {
     void fillPizzaStores() {
 
         pizzaStores.add(new PizzaStore("파파존스", "09:00~21:00", "010-1234-5678", "http://postfiles2.naver.net/20160530_65/ppanppane_1464617766007O9b5u_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%284%29.png?type=w966"));
-        pizzaStores.add(new PizzaStore("피자헛", "09:00~21:00", "010-1234-5678", "https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Pizza_Hut_logo.svg/220px-Pizza_Hut_logo.svg.png"));
-        pizzaStores.add(new PizzaStore("도미노피자", "09:00~21:00", "010-1234-5678", "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Domino%27s_pizza_logo.svg/120px-Domino%27s_pizza_logo.svg.png"));
-        pizzaStores.add(new PizzaStore("미스터피자", "09:00~21:00", "010-1234-5678", "http://postfiles12.naver.net/20160530_171/ppanppane_14646177044221JRNd_PNG/%B9%CC%BD%BA%C5%CD%C7%C7%C0%DA_%B7%CE%B0%ED_%281%29.png?type=w966"));
-        pizzaStores.add(new PizzaStore("피자에땅", "09:00~21:00", "010-1234-5678", "https://mblogthumb-phinf.pstatic.net/20160530_214/ppanppane_1464617804922knKn4_PNG/%C7%C7%C0%DA%BF%A1%B6%A5_%B7%CE%B0%ED_%282%29.png?type=w800"));
-        pizzaStores.add(new PizzaStore("피자스쿨", "09:00~21:00", "010-1234-5678", "https://modo-phinf.pstatic.net/20150501_269/1430484184544WKwLF_JPEG/mosa7NPaR2.jpeg?type=f320_320"));
-        pizzaStores.add(new PizzaStore("피자나라 치킨공주", "09:00~21:00", "010-1234-5678", "http://www.newsworker.co.kr/news/photo/201810/22592_19806_5940.jpg"));
+        pizzaStores.add(new PizzaStore("피자헛", "10:00~21:00", "010-9876-5432", "https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Pizza_Hut_logo.svg/220px-Pizza_Hut_logo.svg.png"));
+        pizzaStores.add(new PizzaStore("도미노피자", "12:00~21:00", "010-2145-7831", "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Domino%27s_pizza_logo.svg/120px-Domino%27s_pizza_logo.svg.png"));
+        pizzaStores.add(new PizzaStore("미스터피자", "11:00~21:00", "010-0587-1234", "http://postfiles12.naver.net/20160530_171/ppanppane_14646177044221JRNd_PNG/%B9%CC%BD%BA%C5%CD%C7%C7%C0%DA_%B7%CE%B0%ED_%281%29.png?type=w966"));
+        pizzaStores.add(new PizzaStore("피자에땅", "09:00~21:00", "010-3481-6247", "https://mblogthumb-phinf.pstatic.net/20160530_214/ppanppane_1464617804922knKn4_PNG/%C7%C7%C0%DA%BF%A1%B6%A5_%B7%CE%B0%ED_%282%29.png?type=w800"));
+        pizzaStores.add(new PizzaStore("피자스쿨", "08:00~21:00", "010-2794-1346", "https://modo-phinf.pstatic.net/20150501_269/1430484184544WKwLF_JPEG/mosa7NPaR2.jpeg?type=f320_320"));
+        pizzaStores.add(new PizzaStore("피자나라 치킨공주", "13:00~21:00", "010-2015-4751", "http://www.newsworker.co.kr/news/photo/201810/22592_19806_5940.jpg"));
     }
 
 
