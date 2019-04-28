e1d99644cb0b9a29db12fca190dd7ac9d59d93ea
kj_cho <kj_cho@idealidea.co.kr>
2019-04-28 16:11:23 +0900

    데이터 넘겨받아서 화면에 뿌리기  - 없는건 더 추가함

--- app/src/main/java/com/tj/pizzastore02/StoreDetailActivity.java
+++ app/src/main/java/com/tj/pizzastore02/StoreDetailActivity.java
@@ -4,9 +4,13 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.bumptech.glide.Glide;
 import com.tj.pizzastore02.databinding.ActivityStoreDetailBinding;
+import com.tj.pizzastore02.datas.PizzaStore;
 
 public class StoreDetailActivity extends BaseActivity {
+
+    PizzaStore storeData;
 
     ActivityStoreDetailBinding act;
 
@@ -25,6 +29,11 @@ public class StoreDetailActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+        storeData = (PizzaStore) getIntent().getSerializableExtra("가게정보");
+
+        act.phoneNumTxt.setText(storeData.phoneNum);
+        act.nameTxt.setText(storeData.name);
+        Glide.with(mContext).load(storeData.logoUrl).into(act.logoImg);
 
     }
 
