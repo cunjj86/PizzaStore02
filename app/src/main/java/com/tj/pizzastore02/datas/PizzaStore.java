6da41b006da7402b3705a00431fa6ccaa4092742
kj_cho <kj_cho@idealidea.co.kr>
2019-04-28 15:34:30 +0900

    피자가게 데이터 클래스 작성

---
+++ app/src/main/java/com/tj/pizzastore02/datas/PizzaStore.java
@@ -0,0 +1,16 @@
+package com.tj.pizzastore02.datas;
+
+public class PizzaStore {
+
+    public String name;
+    public String openTime;
+    public String phoneNum;
+    public String logoUrl;
+
+    public PizzaStore(String name, String openTime, String phoneNum, String logoUrl) {
+        this.name = name;
+        this.openTime = openTime;
+        this.phoneNum = phoneNum;
+        this.logoUrl = logoUrl;
+    }
+}
